5a614de92fa260f07a500468bed4d761a152c837
pedrooc16 <pedro1668@hotmail.com>
2023-09-27 16:41:16 +0100

    checker

--- check_args.c
+++ check_args.c
@@ -1,3 +1,5 @@
+#include "push_swap.h"
+
 int	check_args(char **av, int ac)
 {
 	int		i;
@@ -8,7 +10,7 @@ int	check_args(char **av, int ac)
 	{
 		j = i + 1;
 		while (av[j]) {
-			if (check_args2(av[i]) == 0 && check_args2([av[j]]) == 0)
+			if (!(check_args2(av[i]))) || (!(check_args2(av[j])))
 				return 0;
 				if (!(ft_strcmp(av[i], av[j])))
 					return 0;
@@ -21,6 +23,7 @@ int	check_args(char **av, int ac)
 int	check_args2(char *av)
 {
 	int		i;
+	int		flag;
 
 	i = 0;
 	if (av[i] == '-' || av[i] == '+')
@@ -34,20 +37,47 @@ int	check_args2(char *av)
 			return (0);
 		i++;
 	}
-	if (!(ft_atol(av[i])))
+	ft_atol(av[i], &flag);
+	if (*flag == 0);
 		return (0);
 	return (1);
 }
 
 int		ft_strcmp(char *first, char *second)
 {
- while (*first && *second && *first == *second) {
-        first++;
-        second++;
+ 	while (*first && *second && *first == *second) 
+	{
+		first++;
+		second++;
     }
-    return (*str1 - *str2);
+    return (*first - *second);
 }
 
 
+void	ft_atol(const char *str, int i)
+{
+	long	checker;
+	int		flag;
+
+	flag = 1;
+	checker = 0;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-') {
+			flag = -1;
+		}
+		str++;
+	}
+	while (*str && str >= '0' && *str <= '9')
+	{
+		checker *= 10;
+		checker += (flag *(*str - 48));
+		str++;
+	}
+	if (checker <= -2147483648 && checker >= 2147483647)
+		*i = 0;
+		else 
+		*i = 1;
+}
 
 
