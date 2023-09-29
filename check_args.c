bdea08f1510f3df78b36078f28a80bfcdb3ff63c
pedrooc16 <pedro1668@hotmail.com>
2023-09-29 15:40:01 +0100

    changes

--- check_args.c
+++ check_args.c
@@ -19,89 +19,6 @@ int	ft_isdigit(int c)
 	return (0);
 }
 
-int    check_args(char **av, int i)
-{
-    int        j;
-
-	if (!(check_args2(av[i])))
-		return (0);
-    while (av[i])
-    {
-        j = i + 1;
-        while (av[j]) 
-        {
-            if (!(check_args2(av[j])))
-              return 0;
-                if (!(ft_strcmp(av[i], av[j])))
-                  return 0;
-                j++;    
-        }
-        i++;
-    }
-	return (1);
-}
-
-int	check_args2(char *av)
-{
-	int		i;
-	int		flag;
-
-	i = 0;
-	if (av[i] == '-' || av[i] == '+')
-	{
-		i++;
-	}
-	if ( av[i] == '0' || av[i] == '+' || av[i] == '-')
-	return (0);
-	while (av[i]) 
-	{
-		if (!(ft_isdigit(av[i])))
-			return (0);
-		i++;
-	}
-	ft_atol(av, &flag);
-	if (flag == 0)
-		return (0);
-	return (1);
-}
-
-int		ft_strcmp(char *first, char *second)
-{
- 	while (*first && *second && *first == *second) 
-	{
-		first++;
-		second++;
-    }
-    return (*first - *second);
-}
-
-
-void	ft_atol(const char *str, int * i)
-{
-	long	checker;
-	int		flag;
-
-	flag = 1;
-	checker = 0;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-') {
-			flag = -1;
-		}
-		str++;
-	}
-	while (*str && *str >= '0' && *str <= '9')
-	{
-		checker *= 10;
-		checker += (flag *(*str - 48));
-		str++;
-	}
-	if (checker <= -2147483648 && checker >= 2147483647)
-		*i = 0;
-		else 
-		*i = 1;
-}
-
 static int    word_count(char const *s, char c) 
 {
     int i;
@@ -183,14 +100,100 @@ char	**ft_split(char const *s, char c)
 	return (array);
 }
 
+int    check_args(char **av, int i)
+{
+	int		j;
+
+	printf("%s\n", av[0]);
+	printf("%s\n", av[1]);
+	if (!(check_args2(av[i])))
+		return (0);
+    while (av[i])
+    {
+        j = i + 1;
+        while (av[j]) 
+        {
+            if (!(check_args2(av[j])))
+              return (0);
+                if (!(ft_strcmp(av[i], av[j])))
+                  return (0);
+                j++;    
+        }
+        i++;
+    }
+	return (1);
+}
+
+int	check_args2(char *av)
+{
+	int		i;
+	int		flag;
+
+	i = 0;
+	if (av[i] == '-' || av[i] == '+')
+	{
+		i++;
+	}
+	if ( av[i] == '0' || av[i] == '+' || av[i] == '-')
+	return (0);
+	while (av[i]) 
+	{
+		if (!(ft_isdigit(av[i])))
+			return (0);
+		i++;
+	}
+	ft_atol(av, &flag);
+	if (flag == 0)
+		return (0);
+	return (1);
+}
+
+int		ft_strcmp(char *first, char *second)
+{
+ 	while (*first && *second && *first == *second) 
+	{
+		first++;
+		second++;
+    }
+    return (*first - *second);
+}
+
+
+void	ft_atol(const char *str, int * i)
+{
+	long	checker;
+	int		flag;
+
+	flag = 1;
+	checker = 0;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-') {
+			flag = -1;
+		}
+		str++;
+	}
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		checker *= 10;
+		checker += (flag *(*str - 48));
+		str++;
+	}
+	if (checker <= -2147483648 && checker >= 2147483647)
+		*i = 0;
+		else 
+		*i = 1;
+}
+
+
 int main(int ac, char **av)
 {
-	
-		if (!(count_args(av, ac))){
-		write(1, "error\n", 6);
-		return 0;
+	if (!(count_args(av, ac))){
+		write(1, "error", 4);
+			return 0;
 		}
-	write(1, "successful\n", 11);
+	//printf("successful");
+	write(1, "successful", 11);
 	return 1;
 }
 
@@ -214,6 +217,7 @@ int		count_args(char **av, int ac)
 	}
 	if(!(check_args(args, i)))
 		return (0);
+	return (1);
 }
 
 
